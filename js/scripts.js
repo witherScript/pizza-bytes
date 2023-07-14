e62b2074a2fe1f3da380cb7f57382a95259cbc46
Genesis Scott <chandlerscott123@gmail.com>
2023-07-13 23:10:49 -0500

    format UI logic to process checkbox input through the Pizza object and DOM updates

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,12 @@ function Pizza(toppings, size) {
   this.size = size;
   this.upper = ["pepperoni", "anchovies", "pineapple", "spinach", "parmesean"];
   this.lower = ["cheddar cheese", "feta cheese", "italian sausage", "jalapenos", "bacon"];
+  console.log(toppings);
 }
 
 Pizza.prototype.getPrice = function () {
-  const price = 0.00;
-  this.toppings.array.forEach(topping => {
+  let price = 0.00;
+  this.toppings.forEach(topping => {
     if (this.upper.includes(topping)) {
       price += 1.75;
     }
@@ -31,8 +32,40 @@ Pizza.prototype.getPrice = function () {
     price += 8.25;
   }
 
+  this.price = (price).toFixed(2);
+
 }
 
 //UI Logic
+function displayPizza(pizza) {
+  const order_details = document.querySelector('ul#order');
+  const toppings = pizza.toppings;
+  console.log(toppings);
+  toppings.forEach(function (topping) {
+    console.log(topping);
+    const li = document.createElement('li');
+    li.textContent = topping.charAt(0).toUpperCase() + topping.slice(1);
+    order_details.append(li);
+  });
+  document.querySelector('div#order-form').classList.add('hidden');
+  document.querySelector('span#total').innerText = pizza.price;
+}
 
 
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const toppings = [...document.querySelectorAll('input[name=topping]:checked')].map(e => e.value);
+  const sizeElem = document.getElementById("size").selectedIndex;
+  const size = document.getElementsByTagName("option")[sizeElem].value;
+  const myPizza = new Pizza(toppings, size);
+  myPizza.getPrice();
+  document.querySelector('div#receipt').classList.remove('hidden');
+  displayPizza(myPizza);
+
+}
+
+window.addEventListener('load', function () {
+  const form = document.getElementById('order');
+  form.addEventListener('submit', handleFormSubmit);
+
+});
